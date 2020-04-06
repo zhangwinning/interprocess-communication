1fbe7b23d1151c4f63eebb7b52b0682423890b42
Wenning Zhang <1527841714@qq.com>
2020-04-06 19:08:07 +0800

    add chap-4

---
+++ chap-04/pipe.c
@@ -0,0 +1,80 @@
+#include	<time.h>		/* timespec{} for pselect() */
+#include	<errno.h>
+#include	<fcntl.h>		/* for nonblocking */
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+#include	<unistd.h>
+#include	<sys/wait.h>
+
+#define	MAXLINE		4096	/* max text line length */
+
+void client(int, int), server(int, int);
+int main(int argc, char **argv)
+{
+    int		pipe1[2], pipe2[2];
+    pid_t	childpid;
+
+    pipe(pipe1);	/* create two pipes */
+    pipe(pipe2);
+
+    if ( (childpid = fork()) == 0) {		/* child */
+        close(pipe1[1]);
+        close(pipe2[0]);
+
+        server(pipe1[0], pipe2[1]);
+        exit(0);
+    }
+    /* 4parent */
+    close(pipe1[0]);
+    close(pipe2[1]);
+
+    client(pipe2[0], pipe1[1]);
+
+    waitpid(childpid, NULL, 0);		/* wait for child to terminate */
+    exit(0);
+}
+
+void client(int readfd, int writefd) {
+    size_t len;
+    ssize_t n;
+    char buff[MAXLINE];
+
+    /* 4read pathname */
+    fgets(buff, MAXLINE, stdin);
+    len = strlen(buff);        /* fgets() guarantees null byte at end */
+    if (buff[len - 1] == '\n')
+        len--;                /* delete newline from fgets() */
+
+    /* 4write pathname to IPC channel */
+    write(writefd, buff, len);
+
+    /* 4read from IPC, write to standard output */
+    while ((n = read(readfd, buff, MAXLINE)) > 0)
+        write(STDOUT_FILENO, buff, n);
+}
+
+void server(int readfd, int writefd) {
+    int fd;
+    ssize_t n;
+    char buff[MAXLINE + 1];
+
+    /* 4read pathname from IPC channel */
+    if ((n = read(readfd, buff, MAXLINE)) == 0)
+        printf("end-of-file while reading pathname");
+    buff[n] = '\0';        /* null terminate pathname */
+
+    if ((fd = open(buff, O_RDONLY)) < 0) {
+        /* 4error: must tell client */
+        snprintf(buff + n, sizeof(buff) - n, ": can't open, %s\n",
+                 strerror(errno));
+        n = strlen(buff);
+        write(writefd, buff, n);
+
+    } else {
+        /* 4open succeeded: copy file to IPC channel */
+        while ((n = read(fd, buff, MAXLINE)) > 0)
+            write(writefd, buff, n);
+        close(fd);
+    }
+}
